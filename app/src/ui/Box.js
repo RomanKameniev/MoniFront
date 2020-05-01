3f0626d5267d1e114b924930e39016651c4a5461
Roman Kameniev <roman.kameniev@gmail.com>
2020-05-02 02:40:33 +0300

    -added actions to styled container/text

--- app/src/ui/Box.js
+++ app/src/ui/Box.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const Box = ({ fd = 'row', wrap = false, display = 'flex', w = '100%', h = '100%', bc = '#aaa', children, ...props }) => {
-	return <div style={{ flexDirection: fd, flexWrap: wrap ? 'wrap' : 'nowrap', display, flex: 1, width: w, height: h, backgroundColor: bc, ...props }}>{children}</div>
+const Box = ({ className, onClick, fd = 'row', wrap = false, display = 'flex', w = '100%', h = '100%', bc = '#aaa', children, ...props }) => {
+	return (
+		<div
+			className={className}
+			onClick={onClick}
+			style={{ flexDirection: fd, flexWrap: wrap ? 'wrap' : 'nowrap', display, flex: 1, width: w, height: h, backgroundColor: bc, ...props }}
+		>
+			{children}
+		</div>
+	)
 }
 
 export default Box
--- app/src/ui/StyledText.js
+++ app/src/ui/StyledText.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const StyledText = ({onClick, fontSize = '16px', fontFamily = 'montserrat', fontStyle = 'regular', children, ...props }) => {
-	return <text onClick={onClick} style={{ fontSize, fontFamily, fontStyle, backgroundColor: 'inherit', ...props }}>{children}</text>
+const StyledText = ({ className, onClick, fontSize = '16px', fontFamily = 'montserrat', fontStyle = 'regular', children, ...props }) => {
+	return (
+		<text className={className} onClick={onClick} style={{ fontSize, fontFamily, fontStyle, backgroundColor: 'inherit', ...props }}>
+			{children}
+		</text>
+	)
 }
 
 export default StyledText
