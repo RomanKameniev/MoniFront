6f31f10b820952340012245a1285e29969773bc4
Roman Kameniev <roman.kameniev@gmail.com>
2020-05-03 21:28:02 +0300

    -added action button && updated layout for devices

--- app/src/components/profile/tabsBody/deviceBody.js
+++ app/src/components/profile/tabsBody/deviceBody.js
@@ -5,6 +5,9 @@ import { getDevices } from '../../../api/devices'
 import useAsync from '../../../hooks/useAsync'
 import { errorsNotifHandler } from '../../../utils/notifHendler'
 import Devider from '../../../ui/Devider'
+import Add from '../../../ui/assets/add.svg'
+import Bin from '../../../ui/assets/bin.svg'
+import Edit from '../../../ui/assets/edit.svg'
 
 const DeviceBody = () => {
 	console.log('in devices')
@@ -16,14 +19,14 @@ const DeviceBody = () => {
 
 	return (
 		<Box maxWidth="100%" fd="row" bc="inherit" wrap justifyContent="space-around">
-			<Box maxWidth="30em" bc="inherit" fd="column">
+			<Box maxWidth="40em" bc="inherit" fd="column">
 				<Box bc="#eee" justifyContent="center" borderRadius="10px" marginBottom="5px">
 					<StyledText alignSelf="center">Owned</StyledText>
 				</Box>
 
 				{owned ? (
 					<Box bc="#eee" fd="column">
-						<Box maxWidth="30em" bc="inherit" justifyContent="space-around">
+						<Box maxWidth="40em" bc="inherit" justifyContent="space-around">
 							<StyledText width="10em" margin="5px">
 								Name:
 							</StyledText>
@@ -33,9 +36,12 @@ const DeviceBody = () => {
 							<StyledText width="10em" margin="5px">
 								Id:
 							</StyledText>
+							<StyledText width="10em" margin="5px">
+								Actions:
+							</StyledText>
 						</Box>
 						<Devider />
-						<Box maxWidth="30em" fd="column" bc="inherit" alignItems="flex-start">
+						<Box maxWidth="40em" fd="column" bc="inherit" alignItems="flex-start">
 							{owned.map((i, key) => {
 								return <DeviceCell key={key} {...i} />
 							})}
@@ -45,16 +51,16 @@ const DeviceBody = () => {
 					<StyledText>No owned products</StyledText>
 				)}
 			</Box>
-			<Box maxWidth="30em" bc="inherit" fd="column">
+			<Box maxWidth="40em" bc="inherit" fd="column">
 				<StyledText alignSelf="center">Used</StyledText>
 				{false ? (
 					<Box fd="column" bc="inherit" itemsAlign="flex-start">
-						<Box maxWidth="30em" bc="inherit" justifyContent="space-around">
+						<Box maxWidth="40em" bc="inherit" justifyContent="space-around">
 							<StyledText margin="5px">Name:</StyledText>
 							<StyledText margin="5px">Type:</StyledText>
 							<StyledText margin="5px"> Id:</StyledText>
 						</Box>
-						<Box maxWidth="30em" fd="column" bc="inherit">
+						<Box maxWidth="40em" fd="column" bc="inherit">
 							{owned.map((i, key) => {
 								return <DeviceCell key={key} {...i} />
 							})}
@@ -70,7 +76,7 @@ const DeviceBody = () => {
 
 const DeviceCell = ({ deviceId, type, name }) => {
 	return (
-		<Box maxWidth="30em" justifyContent="flex-start" bc="inherit" textAlign="left">
+		<Box maxWidth="40em" justifyContent="flex-start" bc="inherit" textAlign="left">
 			<StyledText margin="5px" width="10em" textAlign="left">
 				{name}
 			</StyledText>
@@ -80,6 +86,17 @@ const DeviceCell = ({ deviceId, type, name }) => {
 			<StyledText margin="5px" width="10em" textAlign="left">
 				{deviceId}
 			</StyledText>
+			<Box margin="5px" width="10em" alignItems="center" bc="inherit">
+				<Box className="image-action" maxWidth="3em" display="block" max-height="25px" bc="inherit">
+					<img onClick={() => console.log('edit')} src={Edit} style={{ width: '25px' }} alt="edit" />
+				</Box>
+				<Box className="image-action" maxWidth="3em" display="block" max-height="25px" bc="inherit">
+					<img onClick={() => console.log('edit')} src={Add} style={{ width: '25px' }} alt="Add new user" />
+				</Box>{' '}
+				<Box className="image-action" maxWidth="3em" display="block" max-height="25px" bc="inherit">
+					<img onClick={() => console.log('edit')} src={Bin} style={{ width: '25px' }} alt="remove itme" />
+				</Box>
+			</Box>
 		</Box>
 	)
 }
